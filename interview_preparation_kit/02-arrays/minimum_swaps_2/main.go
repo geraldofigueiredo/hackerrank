97b21e147a1c517ce6de2f6ccdd45a45421ae533
geraldofigueiredo <geraldo.figueiredosj@gmail.com>
2020-09-21 23:45:30 -0300

    +1 interview kit

---
+++ interview_preparation_kit/02-arrays/minimum_swaps_2/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the minimumSwaps function below.
+func minimumSwaps(arr []int32) int32 {
+	var count int32 = 0
+	for i := range arr {
+		if arr[i] != int32(i+1) {
+			temp := arr[i]
+			arr[searchElement(arr, i+1)] = temp
+			arr[i] = int32(i + 1)
+			count++
+		}
+	}
+	return count
+}
+
+func searchElement(arr []int32, element int) int {
+	for i := range arr {
+		if arr[i] == int32(element) {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arrTemp := strings.Split(readLine(reader), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	res := minimumSwaps(arr)
+
+	fmt.Fprintf(writer, "%d\n", res)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
